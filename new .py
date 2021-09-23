e2e68e03662fb1ea87775db3efe5f08584127777
Mudit Kumar Singh <ms862977@gmail.com>
2021-09-23 19:47:15 +0530

    Update new .py

--- new .py
+++ new .py
@@ -6,7 +6,7 @@ import datetime
 import pytesseract
 img_cv = cv2.imread(r'/home/pi/pytesseract/tests/data/test.png')
 
-previous ="unkno"
+previous ="unknown"
 
 video_capture = cv2.VideoCapture(0)
 
@@ -20,10 +20,10 @@ while True:
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color 
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
+    
     if process_this_frame:
 
         cv2.imwrite('/home/pi/pytesseract/tests/data/test.png', frame)
@@ -35,14 +35,12 @@ while True:
         espeak.set_voice("En")
         espeak.synth("Hey hello ")
         espeak.synth(pytesseract.image_to_string(img_rgb))
-        # Find all the faces and face encodings in the current frame of video
+        
         cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# Release handle to the webcam
 video_capture.release()
 bus.close()
 cv2.destroyAllWindows()
